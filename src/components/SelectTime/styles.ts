d123a2027b52bd4da047b53f2679b5eca8df07cb
Oleg Razno <razno.oleg@google.com>
2025-03-06 14:02:00 +0200

    Fixed time select

--- src/components/SelectTime/styles.ts
+++ src/components/SelectTime/styles.ts
@@ -4,4 +4,10 @@ export const SelectTimeContainer = styled.div`
   .MuiFormControl-root.MuiTextField-root.css-z3c6am-MuiFormControl-root-MuiTextField-root {
     min-width: 100%;
   }
+
+  @media (min-width: 1200px) {
+    .css-sx5hge {
+      flex-direction: column;
+    }
+  }
 `;
